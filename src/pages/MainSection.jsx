4e07244a9368cbcf6a2b36d3a1a995919d9da455
Alba2809 <197o00679@itsx.edu.mx>
2024-04-15 14:49:32 -0600

    Se agrego la imagen en la bienvenida y se centraron los componentes

--- src/pages/MainSection.jsx
+++ src/pages/MainSection.jsx
@@ -1,5 +1,6 @@
 import { useAuth } from "@context/AuthContext";
 import { useEffect, useState } from "react";
+import { FaUserCircle } from "react-icons/fa";
 
 function WelcomeSection() {
   const { getUser, user } = useAuth();
@@ -15,11 +16,24 @@ function WelcomeSection() {
 
   return (
     <>
-      <header className="h-[50px]">
-        <h1 className="font-medium font-serif text-2xl">
+      <section className="size-full flex flex-col items-center justify-center gap-y-6">
+        {user.imageperfile ? (
+          <div className="rounded-full border-2 border-gray-300 p-1">
+            <img
+              src={user.imageperfile}
+              alt="Image del perfil del usuario"
+              className="min-w-[280px] min-h-[280px] max-w-[280px] max-h-[280px] rounded-full"
+            />
+          </div>
+        ) : (
+          <div className="rounded-full border-2 border-gray-300 p-1">
+            <FaUserCircle color="gray" size="17.5em" />
+          </div>
+        )}
+        <h1 className="font-bold font-serif text-4xl">
           Bienvenido {user?.firstname}!
         </h1>
-      </header>
+      </section>
     </>
   );
 }
